ce2bc812b7b3a4c14b365f626f7ce77f965680b9
Andrew Johnston <johnto20@hotmail.com>
2015-10-17 20:05:06 +1100

    add variable files

---
+++ Intro_to_programming/variables/age.rb
@@ -0,0 +1,7 @@
+puts "How old are you?"
+age = gets.chomp  #note this will ask for age a return it as string
+
+puts "In ten years time you will be #{age.to_i + 10} years old"  #converts string to integer to add to the 10
+puts "In ten years time you will be #{age.to_i + 20} years old"
+puts "In ten years time you will be #{age.to_i + 30} years old"
+puts "In ten years time you will be #{age.to_i + 40} years old"
